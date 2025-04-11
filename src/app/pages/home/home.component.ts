fc91d7199748c8e9db00560ca2f7223c44c8c123
Rabeony Sitrakiniaina Itokiana <rabeitokiana@gmail.com>
2025-04-11 15:41:51 +0100

    component home,skills,navbar,header,experience and install tailwindcss

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,38 @@
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { NavbarComponent } from '../../component/navbar/navbar.component';
+import { HeaderComponent } from '../../component/header/header.component';
+import { SkillsComponent } from '../../component/skills/skills.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    HeaderComponent,
+    SkillsComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements AfterViewInit{
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+
+  }
+  ngAfterViewInit(): void {
+    if(isPlatformBrowser(this.platformId)){
+      gsap.registerPlugin(ScrollTrigger);
+      gsap.to(".txt1",{
+        x: "52vw",
+        duration: 3,
+        scrollTrigger:{
+          trigger : ".txt1",
+          start : "top 80%"
+        } 
+      });
+    }
+  }
+
+}
